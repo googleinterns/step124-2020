febcd9c9fd2fcd8af0184fc44f3ec0dc5b8a80b1
Priya Varra <priyavarra13@gmail.com>
2020-06-24 17:08:56 +0000

    Add querying of places in four directions

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -12,3 +12,80 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
  
+let map;
+
+function createMap() {
+  map = new google.maps.Map(
+    document.getElementById("map-container"),
+    {center: {lat: 36.150813, lng: -40.352239}, zoom: 2});
+}
+
+function findPlaces() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(queryPlaces);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+
+function queryPlaces(position) {
+  //const userLat = position.coords.latitude;
+  //const userLong = position.coords.longitude;
+  
+  const userLat = 37.926916;
+  const userLong = -98.981257;
+
+  const latSpread = 1
+  const longSpread = 2
+
+  queryDirection(userLat - latSpread, userLong); // West
+  queryDirection(userLat, userLong + longSpread); // North
+  queryDirection(userLat + latSpread, userLong); // East
+  queryDirection(userLat, userLong - longSpread); // South
+
+}
+
+function queryDirection(lat, lng) {
+  const sw = new google.maps.LatLng(lat - .4, lng - 4);
+  const ne = new google.maps.LatLng(lat + .4, lng + 4); 
+
+  const boundBox = new google.maps.LatLngBounds(sw, ne);
+
+  const request = {
+    query: 'Tourist Attractions', 
+    bounds: boundBox
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status, pagination) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    
+
+    for (result of results) {
+      if (result.business_status == 'OPERATIONAL') {
+        createMarker(result)
+      }
+    }
+
+    if (pagination.hasNextPage) {
+      pagination.nextPage()
+    }
+
+  }
+}
+
+function createMarker(place) {
+  const marker = new google.maps.Marker({
+    map: map,
+    title: place.name,
+    position: place.geometry.location
+  });
+
+  const infoWindow = new google.maps.InfoWindow({content: place.name});
+
+  marker.addListener("click", () => {infoWindow.open(map, marker)});
+}
