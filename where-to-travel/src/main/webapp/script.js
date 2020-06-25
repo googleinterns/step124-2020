d5d2363c9a6fb7b7920c44d362a57ba7de2ee2aa
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-25 21:16:41 +0000

    Add multiple place functionality

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -1,3 +1,107 @@
+const placeArray = [
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : -33.870775,
+               "lng" : 151.199025
+            }
+         },
+         "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
+         "id" : "21a0b251c9b8392186142c798263e289fe45b4aa",
+         "name" : "Rhythmboat Cruises",
+         "opening_hours" : {
+            "open_now" : true
+         },
+         "photos" : [
+            {
+               "height" : 270,
+               "html_attributions" : [],
+               "photo_reference" : "CnRnAAAAF-LjFR1ZV93eawe1cU_3QNMCNmaGkowY7CnOf-kcNmPhNnPEG9W979jOuJJ1sGr75rhD5hqKzjD8vbMbSsRnq_Ni3ZIGfY6hKWmsOf3qHKJInkm4h55lzvLAXJVc-Rr4kI9O1tmIblblUpg2oqoq8RIQRMQJhFsTr5s9haxQ07EQHxoUO0ICubVFGYfJiMUPor1GnIWb5i8",
+               "width" : 519
+            }
+         ],
+         "place_id" : "ChIJyWEHuEmuEmsRm9hTkapTCrk",
+         "reference" : "CoQBdQAAAFSiijw5-cAV68xdf2O18pKIZ0seJh03u9h9wk_lEdG-cP1dWvp_QGS4SNCBMk_fB06YRsfMrNkINtPez22p5lRIlj5ty_HmcNwcl6GZXbD2RdXsVfLYlQwnZQcnu7ihkjZp_2gk1-fWXql3GQ8-1BEGwgCxG-eaSnIJIBPuIpihEhAY1WYdxPvOWsPnb2-nGb6QGhTipN0lgaLpQTnkcMeAIEvCsSa0Ww",
+         "types" : [ "travel_agency", "restaurant", "food", "establishment" ],
+         "vicinity" : "Pyrmont Bay Wharf Darling Dr, Sydney"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : -33.866891,
+               "lng" : 151.200814
+            }
+         },
+         "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+         "id" : "45a27fd8d56c56dc62afc9b49e1d850440d5c403",
+         "name" : "Private Charter Sydney Habour Cruise",
+         "photos" : [
+            {
+               "height" : 426,
+               "html_attributions" : [],
+               "photo_reference" : "CnRnAAAAL3n0Zu3U6fseyPl8URGKD49aGB2Wka7CKDZfamoGX2ZTLMBYgTUshjr-MXc0_O2BbvlUAZWtQTBHUVZ-5Sxb1-P-VX2Fx0sZF87q-9vUt19VDwQQmAX_mjQe7UWmU5lJGCOXSgxp2fu1b5VR_PF31RIQTKZLfqm8TA1eynnN4M1XShoU8adzJCcOWK0er14h8SqOIDZctvU",
+               "width" : 640
+            }
+         ],
+         "place_id" : "ChIJqwS6fjiuEmsRJAMiOY9MSms",
+         "reference" : "CpQBhgAAAFN27qR_t5oSDKPUzjQIeQa3lrRpFTm5alW3ZYbMFm8k10ETbISfK9S1nwcJVfrP-bjra7NSPuhaRulxoonSPQklDyB-xGvcJncq6qDXIUQ3hlI-bx4AxYckAOX74LkupHq7bcaREgrSBE-U6GbA1C3U7I-HnweO4IPtztSEcgW09y03v1hgHzL8xSDElmkQtRIQzLbyBfj3e0FhJzABXjM2QBoUE2EnL-DzWrzpgmMEulUBLGrtu2Y",
+         "types" : [ "restaurant", "food", "establishment" ],
+         "vicinity" : "Australia"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : -33.870943,
+               "lng" : 151.190311
+            }
+         },
+         "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+         "id" : "30bee58f819b6c47bd24151802f25ecf11df8943",
+         "name" : "Bucks Party Cruise",
+         "opening_hours" : {
+            "open_now" : true
+         },
+         "photos" : [
+            {
+               "height" : 600,
+               "html_attributions" : [],
+               "photo_reference" : "CnRnAAAA48AX5MsHIMiuipON_Lgh97hPiYDFkxx_vnaZQMOcvcQwYN92o33t5RwjRpOue5R47AjfMltntoz71hto40zqo7vFyxhDuuqhAChKGRQ5mdO5jv5CKWlzi182PICiOb37PiBtiFt7lSLe1SedoyrD-xIQD8xqSOaejWejYHCN4Ye2XBoUT3q2IXJQpMkmffJiBNftv8QSwF4",
+               "width" : 800
+            }
+         ],
+         "place_id" : "ChIJLfySpTOuEmsRsc_JfJtljdc",
+         "reference" : "CoQBdQAAANQSThnTekt-UokiTiX3oUFT6YDfdQJIG0ljlQnkLfWefcKmjxax0xmUpWjmpWdOsScl9zSyBNImmrTO9AE9DnWTdQ2hY7n-OOU4UgCfX7U0TE1Vf7jyODRISbK-u86TBJij0b2i7oUWq2bGr0cQSj8CV97U5q8SJR3AFDYi3ogqEhCMXjNLR1k8fiXTkG2BxGJmGhTqwE8C4grdjvJ0w5UsAVoOH7v8HQ",
+         "types" : [ "restaurant", "food", "establishment" ],
+         "vicinity" : "37 Bank St, Pyrmont"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : -33.867591,
+               "lng" : 151.201196
+            }
+         },
+         "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
+         "id" : "a97f9fb468bcd26b68a23072a55af82d4b325e0d",
+         "name" : "Australian Cruise Group",
+         "opening_hours" : {
+            "open_now" : true
+         },
+         "photos" : [
+            {
+               "height" : 242,
+               "html_attributions" : [],
+               "photo_reference" : "CnRnAAAABjeoPQ7NUU3pDitV4Vs0BgP1FLhf_iCgStUZUr4ZuNqQnc5k43jbvjKC2hTGM8SrmdJYyOyxRO3D2yutoJwVC4Vp_dzckkjG35L6LfMm5sjrOr6uyOtr2PNCp1xQylx6vhdcpW8yZjBZCvVsjNajLBIQ-z4ttAMIc8EjEZV7LsoFgRoU6OrqxvKCnkJGb9F16W57iIV4LuM",
+               "width" : 200
+            }
+         ],
+         "place_id" : "ChIJrTLr-GyuEmsRBfy61i59si0",
+         "reference" : "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
+         "types" : [ "travel_agency", "restaurant", "food", "establishment" ],
+         "vicinity" : "32 The Promenade, King Street Wharf 5, Sydney"
+      }
+   ];
+
 let mapStyles = [
   {
     "featureType": "landscape",
@@ -27,18 +131,17 @@ let mapStyles = [
 ];
 
 function initialize() {
-let home = {lat: 39.749, lng: -104.994};
-        let place = {lat: 39.8, lng: -104.994};
-    var mapOptions = {
-            center: new google.maps.LatLng(40.435833800555567, -78.44189453125),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoom: 11,
-            mapTypeControl: false,
-            styles: myStyles
-          };	 
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  let home = {lat: -33.86, lng: 151.2027};
+  let mapOptions = {
+    center: new google.maps.LatLng(-33.868, 151.2),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 16,
+    mapTypeControl: false,
+    styles: mapStyles
+  };	 
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  var contentString = 
+  let contentString = 
     '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
@@ -49,26 +152,29 @@ let home = {lat: 39.749, lng: -104.994};
       '</div>'+
     '</div>';
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var placeMarker = new google.maps.Marker({
-    position: place,
-    map: map,
-    title: 'A place'
-  });
-
-  var homeMarker = new google.maps.Marker({
+  let homeMarker = new google.maps.Marker({
     position: home,
     map: map,
-    title: 'A place'
-  });
-
-  placeMarker.addListener('click', function() {
-    infowindow.open(map, placeMarker);
+    title: 'Home'
   });
   
-  console.log(places);
+  let i;
+  for(i = 0; i < placeArray.length; i++) {
+    let name = placeArray[i].name;
+    let coordinates = placeArray[i].geometry.location;
+    let placeMarker = new google.maps.Marker({
+      position: coordinates,
+      map: map,
+      title: name
+    });
+
+    let infowindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    placeMarker.addListener('click', function() {
+      infowindow.open(map, placeMarker);
+    });
+  }
 }
  
