db25d0b495c53400dbfd4c91a44619be422e438d
Priya Varra <priyavarra@google.com>
2020-06-25 18:21:28 +0000

    Add colllection of all places into one array and support for user inputting location

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -14,70 +14,138 @@
  
 let map;
 
+/* Creates initial map */
 function createMap() {
   map = new google.maps.Map(
     document.getElementById("map-container"),
     {center: {lat: 36.150813, lng: -40.352239}, zoom: 2});
 }
 
-function findPlaces() {
+/** 
+ * Obtains user location if they give permission otherwise asks
+ * for address from user before  querying Places API for locations
+ * that fall within time provided by user.
+ *
+ * @param {string} time Amount of travel time requested by user
+ */
+function findPlaces(time) {
+  function success(position) {
+    queryPlaces(position, time);
+  }
+
+  function error() {
+    queryFromInputLocation(time);
+  }
+
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(queryPlaces);
+    navigator.geolocation.getCurrentPosition(success, error);
   } else {
-    alert("Geolocation is not supported by this browser.");
+    queryFromInputLocation(time);
   }
 }
 
+/** 
+ * If browser does not support geolocation or user does not provide permissions,
+ * asks user to input address and utilizes Geocoding API to convert address to
+ * latitude and longitude. User will be prompted until they provide a valid address.
+ * 
+ * @param {string} time Amount of travel time requested by user
+ */
+function queryFromInputLocation(time) {
+  const address = prompt("Please enter a valid address as your start location.");
+  if (address == null || address == "") {
+    queryFromInputLocation(time);
+  }
+ 
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status == 'OK') {
+      const lat = results[0].geometry.location.lat;
+      const lng = results[0].geometry.location.lng;
+      const position = {coords: {latitude: lat() , longitude: lng()}};
 
-function queryPlaces(position) {
+      queryPlaces(position, time);
+    } else {
+      queryFromInputLocation(time);
+    }
+  });
+
+}
+
+/** 
+ * Finds places centered around user's position and passes to filter function
+ * to return places that are close to travel time requested by user. Uses four
+ * bounding boxes that lie north, south, east, and west of the user's location.
+ *
+ * @param {object} position Contains user's latitude and longitude
+ * @param {string} time Amount of travel time requested by user
+ */
+async function queryPlaces(position, time) {
   //const userLat = position.coords.latitude;
-  //const userLong = position.coords.longitude;
+  //const userLng = position.coords.longitude;
   
+  // Center of Kansas
   const userLat = 37.926916;
-  const userLong = -98.981257;
+  const userLng = -98.981257;
 
-  const latSpread = 1
-  const longSpread = 2
+  const userLocation = new google.maps.LatLng(userLat, userLng);
+  
+  // These spread the search area for the four bounding boxes
+  const latSpread = 2
+  const lngSpread = 8
 
-  queryDirection(userLat - latSpread, userLong); // West
-  queryDirection(userLat, userLong + longSpread); // North
-  queryDirection(userLat + latSpread, userLong); // East
-  queryDirection(userLat, userLong - longSpread); // South
+  let place_candidates = [];
+  place_candidates = await queryDirection(userLat - latSpread, userLng, place_candidates); // West
+  place_candidates = await queryDirection(userLat, userLng + lngSpread, place_candidates); // North
+  place_candidates = await queryDirection(userLat + latSpread, userLng, place_candidates); // East
+  place_candidates = await queryDirection(userLat, userLng - lngSpread, place_candidates); // South
 
+  return filterByDistance(time, place_candidates, userLocation);
 }
 
-function queryDirection(lat, lng) {
-  const sw = new google.maps.LatLng(lat - .4, lng - 4);
-  const ne = new google.maps.LatLng(lat + .4, lng + 4); 
+/** 
+ * Finds places constrained to bounding box centered at provided latitude and longitude.
+ * Adds all places with operational business status to array of place candidates.
+ *
+ * @param {number} lat Bounding box center latitude
+ * @param {number} lng Bounding box center longitude
+ * @param {array} place_candidates Array of place objects
+ */
+function queryDirection(lat, lng, place_candidates) {
+  return new Promise(function(resolve, reject) {
+    const sw = new google.maps.LatLng(lat - .4, lng - 4);
+    const ne = new google.maps.LatLng(lat + .4, lng + 4); 
 
-  const boundBox = new google.maps.LatLngBounds(sw, ne);
+    const boundBox = new google.maps.LatLngBounds(sw, ne);
 
-  const request = {
-    query: 'Tourist Attractions', 
-    bounds: boundBox
-  };
+    const request = {
+      query: 'Tourist Attractions', 
+      bounds: boundBox
+    };
 
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
-
-function callback(results, status, pagination) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    
-
-    for (result of results) {
-      if (result.business_status == 'OPERATIONAL') {
-        createMarker(result)
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (result of results) {
+          if (result.business_status == 'OPERATIONAL') {
+            place_candidates.push(result);
+            createMarker(result)
+          }
+        }
       }
+
+      resolve(place_candidates);
     }
 
-    if (pagination.hasNextPage) {
-      pagination.nextPage()
-    }
-
-  }
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+  });
 }
 
+/**
+* Creates marker from place object to display on map 
+*
+* @param {object} place Place object containing name and location 
+*/
 function createMarker(place) {
   const marker = new google.maps.Marker({
     map: map,
@@ -88,4 +156,8 @@ function createMarker(place) {
   const infoWindow = new google.maps.InfoWindow({content: place.name});
 
   marker.addListener("click", () => {infoWindow.open(map, marker)});
+}
+
+function filterByDistance(time, place_candidates, userLocation) {
+    return;
 }
