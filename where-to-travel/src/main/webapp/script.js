80077f0777a3c071ba2050935569a2685427b043
Priya Varra <priyavarra@google.com>
2020-06-25 23:01:03 +0000

    Change functions to integrate with frontend

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -12,49 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
  
-let map;
-
-/* Creates initial map */
-function createMap() {
-  map = new google.maps.Map(
-    document.getElementById("map-container"),
-    {center: {lat: 36.150813, lng: -40.352239}, zoom: 2});
-}
-
 /** 
- * Obtains user location if they give permission otherwise asks
- * for address from user before  querying Places API for locations
- * that fall within time provided by user.
+ * If browser supports geolocation and user provides permissions, obtains user's 
+ * latitude and longitude. Otherwise, asks user to input address and converts input
+ * to latitude and longitude.
  *
- * @param {string} time Amount of travel time requested by user
+ * @return {Object} Contains latitude and longitude corresponding to user's location
  */
-function findPlaces(time) {
-  function success(position) {
-    queryPlaces(position, time);
+function getUserLocation() {
+    function success(position) {
+    return {lat: position.coords.latitude, lng: position.coords.longitude};
   }
 
   function error() {
-    queryFromInputLocation(time);
+    getLocationFromUserInput();
   }
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
   } else {
-    queryFromInputLocation(time);
+    getLocationFromUserInput();
   }
 }
+
 
 /** 
  * If browser does not support geolocation or user does not provide permissions,
  * asks user to input address and utilizes Geocoding API to convert address to
  * latitude and longitude. User will be prompted until they provide a valid address.
- * 
- * @param {string} time Amount of travel time requested by user
+ *
+ * @return {Object} Contains latitude and longitude corresponding to input address
  */
-function queryFromInputLocation(time) {
+function getLocationFromUserInput() {
   const address = prompt("Please enter a valid address as your start location.");
   if (address == null || address == "") {
-    queryFromInputLocation(time);
+    queryFromInputLocation();
   }
  
   const geocoder = new google.maps.Geocoder();
@@ -62,34 +54,28 @@ function queryFromInputLocation(time) {
     if (status == 'OK') {
       const lat = results[0].geometry.location.lat;
       const lng = results[0].geometry.location.lng;
-      const position = {coords: {latitude: lat() , longitude: lng()}};
-
-      queryPlaces(position, time);
+      return {lat: lat() , lng: lng()};  
     } else {
-      queryFromInputLocation(time);
+      queryFromInputLocation();
     }
   });
-
 }
 
 /** 
  * Finds places centered around user's position and passes to filter function
  * to return places that are close to travel time requested by user. Uses four
- * bounding boxes that lie north, south, east, and west of the user's location.
+ * bounding boxes that lie north, south, east, and west of user's location.
  *
- * @param {object} position Contains user's latitude and longitude
- * @param {string} time Amount of travel time requested by user
+ * @param {object} timeObj Travel time requested by user in hours and mins
  */
-async function queryPlaces(position, time) {
-  //const userLat = position.coords.latitude;
-  //const userLng = position.coords.longitude;
+async function getPlacesFromTime(timeObj) {
+  //const userLat = home.lat;
+  //const userLng = home.lng;
   
   // Center of Kansas
   const userLat = 37.926916;
   const userLng = -98.981257;
 
-  const userLocation = new google.maps.LatLng(userLat, userLng);
-  
   // These spread the search area for the four bounding boxes
   const latSpread = 2
   const lngSpread = 8
@@ -100,16 +86,18 @@ async function queryPlaces(position, time) {
   place_candidates = await queryDirection(userLat + latSpread, userLng, place_candidates); // East
   place_candidates = await queryDirection(userLat, userLng - lngSpread, place_candidates); // South
 
-  return filterByDistance(time, place_candidates, userLocation);
+  // TODO: Call Emma's function to get list of places that are within time 
+  // return filterByDistance(timeObj, place_candidates);
 }
 
 /** 
- * Finds places constrained to bounding box centered at provided latitude and longitude.
- * Adds all places with operational business status to array of place candidates.
+ * Finds tourist attractions constrained to bounding box centered at provided latitude
+ * and longitude. Adds all places with operational business status to array of place candidates.
  *
  * @param {number} lat Bounding box center latitude
  * @param {number} lng Bounding box center longitude
  * @param {array} place_candidates Array of place objects
+ * @return {Promise} An array promise of added place candidates
  */
 function queryDirection(lat, lng, place_candidates) {
   return new Promise(function(resolve, reject) {
@@ -139,25 +127,4 @@ function queryDirection(lat, lng, place_candidates) {
     service = new google.maps.places.PlacesService(map);
     service.textSearch(request, callback);
   });
-}
-
-/**
-* Creates marker from place object to display on map 
-*
-* @param {object} place Place object containing name and location 
-*/
-function createMarker(place) {
-  const marker = new google.maps.Marker({
-    map: map,
-    title: place.name,
-    position: place.geometry.location
-  });
-
-  const infoWindow = new google.maps.InfoWindow({content: place.name});
-
-  marker.addListener("click", () => {infoWindow.open(map, marker)});
-}
-
-function filterByDistance(time, place_candidates, userLocation) {
-    return;
 }
