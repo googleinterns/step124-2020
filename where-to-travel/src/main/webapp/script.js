53a7c26f58ed43151612c3e7410a6bef96eb11b7
emmapaczkowski <emmapaczkowski@gmail.com>
2020-06-26 04:12:40 +0000

    Delete comments.

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -11,10 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- function filterByDistance(time, listPlaces, userLocation) {
-  //final list of places accociated with times
-  var acceptablePlaces = {};
 
+ function filterByDistance(time, listPlaces, userLocation) {
+  
+  
+  var acceptablePlaces = {};
   var userDestinations;
 
   //itterate through listPlaces and to get all the destinations
