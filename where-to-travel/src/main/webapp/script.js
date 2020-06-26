595310a249128fc5b2b110eba2e640180d5f4fa3
emmapaczkowski <emmapaczkowski@gmail.com>
2020-06-26 04:04:41 +0000

    Add filterByDistance first draft

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -11,41 +11,51 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
- function filterByDistance( time, listPlaces, userLocation) {
+ function filterByDistance(time, listPlaces, userLocation) {
+  //final list of places accociated with times
+  var acceptablePlaces = {};
 
- }
- 
-var origin1 = new google.maps.LatLng(55.930385, -3.118425);
-var origin2 = 'Greenwich, England';
-var destinationA = 'Stockholm, Sweden';
-var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+  var userDestinations;
 
-var service = new google.maps.DistanceMatrixService();
-service.getDistanceMatrix(
-  {
-    origins: [origin1, origin2],
-    destinations: [destinationA, destinationB],
+  //itterate through listPlaces and to get all the destinations
+  for (var i = 0; i < listPlaces.length; i++) {
+    userDestinations.push(new google.maps.LatLng(listPlaces[i].location.lat, listPlaces[i].location.lng))
+  }
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix({
+    origins: [userLocaiton],
+    destinations: userDestinations,
     travelMode: 'DRIVING',
     transitOptions: TransitOptions,
     drivingOptions: DrivingOptions,
     unitSystem: google.maps.UnitSystem.IMPERIAL,
   }, callback);
 
-function callback(response, status) {
-  if (status == 'OK') {
-    var origins = response.originAddresses;
-    var destinations = response.destinationAddresses;
+  function callback(response, status) {
+    if (status == 'OK') {
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
 
-    for (var i = 0; i < origins.length; i++) {
-      var results = response.rows[i].elements;
-      for (var j = 0; j < results.length; j++) {
-        var element = results[j];
-        var distance = element.distance.text;
-        var duration = element.duration.text;
-        var from = origins[i];
-        var to = destinations[j];
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+        for (var j = 0; j < results.length; j++) {
+          var element = results[j];
+          var distance = element.distance.text;
+          var duration = element.duration.text;
+          var from = origins[i];
+          var to = destinations[j];
+
+          //Check if the time is within the +- 30 min = 1800 sec range
+          if (element.duration.value < time + 1800 && element.duration.value > time - 1800) {
+            acceptablePlaces.push({
+              "name": listPlaces[j].name,
+              "timeInSeconds": element.duration.value,
+              "timeAsString": element.duration.text
+            })
+          }
+        }
       }
     }
   }
+  return acceptablePlaces;
 }
