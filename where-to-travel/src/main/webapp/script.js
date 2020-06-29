0f114e80d13b5bb1b0efc748318577b0ebe86168
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-29 21:48:07 +0000

    fix merge

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -1,30 +1,30 @@
 //This is map stylings for the GMap api
 const mapStyles = [
   {
-    "featureType": "landscape",
-    "stylers": [
+    featureType: "landscape",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit",
-    "stylers": [
+    featureType: "transit",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
-  }
+        visibility: "off",
+      },
+    ],
+  },
 ];
 //End map stylings
 
@@ -36,7 +36,6 @@ function openNav() {
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   mapOverlay = document.getElementById("over_map");
   mapOverlay.style.zIndex = "5";
-  mapOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
   mapOverlay.style.transition = "background-color .5s";
   mapOverlay.style.backgroundColor = "rgba(0,0,0,0.4)";
   open = true;
@@ -44,15 +43,17 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
   mapOverlay = document.getElementById("over_map");
   mapOverlay.style.transition = "background-color .5s";
   mapOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
   open = false;
-  mapOverlay.addEventListener(
-    'transitionend', (event) => { if (!open) { event.target.style.zIndex = "0";} }
-  );
+  mapOverlay.addEventListener("transitionend", (event) => {
+    if (!open) {
+      event.target.style.zIndex = "0";
+    }
+  });
 }
 
 const submitId = "submit-id";
@@ -63,9 +64,11 @@ let map;
 let home = null;
 
 // Add gmap js library to head of page
-let script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=' 
-              + secrets['googleMapsKey'] + '&libraries=places';
+let script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=" +
+  secrets["googleMapsKey"] +
+  "&libraries=places";
 script.defer = true;
 script.async = true;
 
@@ -76,24 +79,24 @@ document.head.appendChild(script);
  */
 async function initialize() {
   let submit = document.getElementById(submitId);
-  submit.addEventListener('click', submitDataListener);
-  home = await getUserLocation();    
+  submit.addEventListener("click", submitDataListener);
+  home = await getUserLocation();
 
   const mapOptions = {
-    center: home, 
+    center: home,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 16,
     mapTypeControl: false,
-    styles: mapStyles
+    styles: mapStyles,
   };
-  
+
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   let homeMarker = new google.maps.Marker({
     position: home,
     icon: "icons/home.svg",
     map: map,
-    title: 'Home'
+    title: "Home",
   });
 }
 
@@ -105,8 +108,8 @@ async function initialize() {
 function submitDataListener(event) {
   const hours = document.getElementById(hoursId).value;
   const minutes = document.getElementById(minutesId).value;
-  const timeObj = { "hours": hours, "minutes": minutes };
-  getPlacesFromTime(timeObj).then(places => populatePlaces(places));
+  const timeObj = { hours: hours, minutes: minutes };
+  getPlacesFromTime(timeObj).then((places) => populatePlaces(places));
 }
 
 /**
@@ -117,44 +120,45 @@ function submitDataListener(event) {
  */
 function populatePlaces(placeArray) {
   let i;
-  for(i = 0; i < placeArray.length; i++) {
+  for (i = 0; i < placeArray.length; i++) {
     let name = placeArray[i].name;
     let coordinates = placeArray[i].geometry.location;
     let placeMarker = new google.maps.Marker({
       position: coordinates,
       map: map,
       title: name,
-      icon: "icons/pin.svg"
+      icon: "icons/pin.svg",
     });
 
     let infowindow = new google.maps.InfoWindow({
-      content: name
+      content: name,
     });
 
-    placeMarker.addListener('click', function() {
+    placeMarker.addListener("click", function () {
       infowindow.open(map, placeMarker);
     });
   }
 }
- 
 
-/** 
- * If browser supports geolocation and user provides permissions, obtains user's 
+/**
+ * If browser supports geolocation and user provides permissions, obtains user's
  * latitude and longitude. Otherwise, asks user to input address and converts input
  * to latitude and longitude.
  *
  * @return {Object} Contains latitude and longitude corresponding to user's location
  */
- function getUserLocation() {
-  return new Promise(function(resolve, reject) {
-  
+function getUserLocation() {
+  return new Promise(function (resolve, reject) {
     function success(position) {
-      return resolve({lat: position.coords.latitude, lng: position.coords.longitude});
+      return resolve({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
     }
 
-   function error() {
-     return resolve(getLocationFromUserInput());
-   }
+    function error() {
+      return resolve(getLocationFromUserInput());
+    }
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
@@ -164,8 +168,7 @@ function populatePlaces(placeArray) {
   });
 }
 
-
-/** 
+/**
  * If browser does not support geolocation or user does not provide permissions,
  * asks user to input address and utilizes Geocoding API to convert address to
  * latitude and longitude. User will be prompted until they provide a valid address.
@@ -173,18 +176,20 @@ function populatePlaces(placeArray) {
  * @return {Object} Contains latitude and longitude corresponding to input address
  */
 function getLocationFromUserInput() {
-  return new Promise(function(resolve, reject) {
-    const address = prompt("Please enter a valid address as your start location.");
+  return new Promise(function (resolve, reject) {
+    const address = prompt(
+      "Please enter a valid address as your start location."
+    );
     if (address == null || address == "") {
       return resolve(getLocationFromUserInput());
     }
- 
+
     const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status == 'OK') {
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status == "OK") {
         const lat = results[0].geometry.location.lat;
         const lng = results[0].geometry.location.lng;
-        return resolve({lat: lat() , lng: lng()});  
+        return resolve({ lat: lat(), lng: lng() });
       } else {
         return resolve(getLocationFromUserInput());
       }
@@ -192,32 +197,47 @@ function getLocationFromUserInput() {
   });
 }
 
-/** 
+/**
  * Finds places centered around user's position and passes to filter function
  * to return places that are close to travel time requested by user. Uses four
  * bounding boxes that lie north, south, east, and west of user's location.
  *
  * @param {object} timeObj Travel time requested by user in hours and minutes
  */
- async function getPlacesFromTime(timeObj) {
+async function getPlacesFromTime(timeObj) {
   const userLat = home.lat;
   const userLng = home.lng;
-  
+
   // These spread the search area for the four bounding boxes
-  const latSpread = 2
-  const lngSpread = 8
+  const latSpread = 2;
+  const lngSpread = 8;
 
   let place_candidates = [];
-  place_candidates = await addPlacesFromDirection(userLat - latSpread, userLng, place_candidates); // West
-  place_candidates = await addPlacesFromDirection(userLat, userLng + lngSpread, place_candidates); // North
-  place_candidates = await addPlacesFromDirection(userLat + latSpread, userLng, place_candidates); // East
-  place_candidates = await addPlacesFromDirection(userLat, userLng - lngSpread, place_candidates); // South
-
+  place_candidates = await addPlacesFromDirection(
+    userLat - latSpread,
+    userLng,
+    place_candidates
+  ); // West
+  place_candidates = await addPlacesFromDirection(
+    userLat,
+    userLng + lngSpread,
+    place_candidates
+  ); // North
+  place_candidates = await addPlacesFromDirection(
+    userLat + latSpread,
+    userLng,
+    place_candidates
+  ); // East
+  place_candidates = await addPlacesFromDirection(
+    userLat,
+    userLng - lngSpread,
+    place_candidates
+  ); // South
 
   return filterByDistance(timeObj, place_candidates);
 }
 
-/** 
+/**
  * Finds tourist attractions constrained to bounding box centered at provided latitude
  * and longitude. Adds all places with operational business status to array of place candidates.
  *
@@ -227,24 +247,24 @@ function getLocationFromUserInput() {
  * @return {Promise} An array promise of added place candidates
  */
 function addPlacesFromDirection(lat, lng, place_candidates) {
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     const halfWidth = 0.5;
     const halfHeight = 0.5;
-    
+
     const sw = new google.maps.LatLng(lat - halfWidth, lng - halfHeight);
-    const ne = new google.maps.LatLng(lat + halfWidth, lng + halfHeight); 
+    const ne = new google.maps.LatLng(lat + halfWidth, lng + halfHeight);
 
     const boundBox = new google.maps.LatLngBounds(sw, ne);
 
     const request = {
-      query: 'Tourist Attractions', 
-      bounds: boundBox
+      query: "Tourist Attractions",
+      bounds: boundBox,
     };
 
     function callback(results, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (result of results) {
-          if (result.business_status == 'OPERATIONAL') {
+          if (result.business_status == "OPERATIONAL") {
             place_candidates.push(result);
           }
         }
@@ -258,21 +278,21 @@ function addPlacesFromDirection(lat, lng, place_candidates) {
   });
 }
 
-/** 
+/**
  * Filter through tourist attractions to find which are in the given time frame of the user.
  *
  * @param {number} time How much time the user wants to travel for
  * @param {array} listPlaces Array of place objects
  * @return {array} An array of places objects that are in the given time frame
  */
- function filterByDistance(timeObj, listPlaces) {
-  return new Promise(function(resolve) {
+function filterByDistance(timeObj, listPlaces) {
+  return new Promise(function (resolve) {
     const userLocation = new google.maps.LatLng(home.lat, home.lng);
     let userDestinations = [];
     let acceptablePlaces = [];
 
     const time = timeObj.hours * 3600 + timeObj.minutes * 60;
-    
+
     //iterate through listPlaces and to get all the destinations
     let i;
     for (i = 0; i < 25; i++) {
@@ -283,17 +303,20 @@ function addPlacesFromDirection(lat, lng, place_candidates) {
     }
 
     let service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix({
-      origins: [userLocation],
-      destinations: userDestinations,
-      travelMode: 'DRIVING',
-      unitSystem: google.maps.UnitSystem.IMPERIAL,
-    }, callback);
+    service.getDistanceMatrix(
+      {
+        origins: [userLocation],
+        destinations: userDestinations,
+        travelMode: "DRIVING",
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+      },
+      callback
+    );
 
-    function callback(response, status) {    
-      if (status == 'OK') {
+    function callback(response, status) {
+      if (status == "OK") {
         const origins = response.originAddresses;
-        
+
         let i;
         for (i = 0; i < origins.length; i++) {
           const results = response.rows[i].elements;
@@ -301,12 +324,15 @@ function addPlacesFromDirection(lat, lng, place_candidates) {
           for (j = 0; j < results.length; j++) {
             const element = results[j];
             //Check if the time is within the +- 30 min = 1800 sec range
-            if (element.duration.value < time + 1800 && element.duration.value > time - 1800) {
+            if (
+              element.duration.value < time + 1800 &&
+              element.duration.value > time - 1800
+            ) {
               acceptablePlaces.push({
-                "name": listPlaces[j].name,
-                "geometry" : listPlaces[j].geometry,
-                "timeInSeconds": element.duration.value,
-                "timeAsString": element.duration.text
+                name: listPlaces[j].name,
+                geometry: listPlaces[j].geometry,
+                timeInSeconds: element.duration.value,
+                timeAsString: element.duration.text,
               });
             }
           }
