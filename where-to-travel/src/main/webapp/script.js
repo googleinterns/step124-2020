e7d93237731f07c36af8f41789dd1f3db4c5c10d
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-29 19:10:19 +0000

    Add switch boolean to drawer

--- where-to-travel/src/main/webapp/script.js
+++ where-to-travel/src/main/webapp/script.js
@@ -28,22 +28,31 @@ const mapStyles = [
 ];
 //End map stylings
 
+let open = false;
 // Need to blur header and blur mapwrap
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   mapOverlay = document.getElementById("over_map");
-  mapOverlay.style.opacity = "1";
-  mapOverlay.style.backgroundColor = "rgba(0,0,0,0)";
+  mapOverlay.style.zIndex = "5";
+  mapOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
   mapOverlay.style.transition = "background-color .5s";
   mapOverlay.style.backgroundColor = "rgba(0,0,0,0.4)";
+  open = true;
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
+  mapOverlay = document.getElementById("over_map");
+  mapOverlay.style.transition = "background-color .5s";
+  mapOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  open = false;
+  mapOverlay.addEventListener(
+    'transitionend', (event) => { if (!open) { event.target.style.zIndex = "0";} }
+  );
 }
 
 const submitId = "submitId";
