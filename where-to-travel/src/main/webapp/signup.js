3cbb0388cbac2c2112c2faf655e820efa190efba
emmapaczkowski <emmapaczkowski@gmail.com>
2020-07-09 15:16:51 +0000

    Add notifications for sign up.

--- where-to-travel/src/main/webapp/signup.js
+++ where-to-travel/src/main/webapp/signup.js
@@ -1,4 +1,3 @@
-
 // Get elements for authentication
 const emailSignUp = document.getElementById('emailSignUp');
 const passwordSignUp = document.getElementById('passwordSignUp');
@@ -10,9 +9,22 @@ btnSignUp.addEventListener('click', e => {
   //TODO: Check for real emails
   const email = emailSignUp.value;
   const pass = passwordSignUp.value;
+  const passConfirmation = passwordConfirmation.value;
   const auth = firebase.auth();
 
-  // Sign up
-  const promise = auth.createUserWithEmailAndPassword(email, pass);
-  promise.catch(e => console.log(e.message));
+  if(pass != passConfirmation) {
+    alert("Your passwords do not match. Please try again.");
+  }
+  // Passwords must be at least 6 characters in length
+  else if(pass.length < 6) {
+    alert("Your password must be at least 6 characters long. Please try again");
+  }
+  else {
+    // Sign up
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.then(e => {
+      alert("You have sucessfully signed up!")
+    });
+    promise.catch(e => console.log(e.message));
+  }
 });
