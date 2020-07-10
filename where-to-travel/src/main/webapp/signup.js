31e55eb1ca54580d07a3e08261eac92a0b51e910
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-07-10 19:38:53 +0000

    Add sign up validation

--- where-to-travel/src/main/webapp/signup.js
+++ where-to-travel/src/main/webapp/signup.js
@@ -5,10 +5,38 @@ const passwordSignUp = document.getElementById('passwordSignUp');
 const passwordConfirmation = document.getElementById('passwordConfirmation');
 const btnSignUp = document.getElementById('signUp');
 
+// Preferred method of material form behavior interaction is jQuery
+// When the document is loaded, add validity check to form on submit.
+$(document).ready(function() {
+    $('#signUpForm').submit(function() {
+      // if form is invalid, stop event
+      if (!passwordConfirmation.validity.valid) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        signUp();
+        // send to home page
+        window.location.href = 'index.html';
+      }
+      $('#signUpForm').addClass('was-validated');
+    });
+});
+
+function validatePassword(){
+  if(passwordSignUp.value != passwordConfirmation.value) {
+    passwordConfirmation.setCustomValidity('x');
+  } else {
+    passwordConfirmation.setCustomValidity('');
+  }
+}
+
+passwordSignUp.onchange = validatePassword;
+passwordConfirmation.onkeyup = validatePassword;
+
 
 
 // Add signup event
-btnSignUp.addEventListener('click', e => {
+function signUp() {
   //TODO: Check for real emails
   const email = emailSignUp.value;
   const pass = passwordSignUp.value;
@@ -17,4 +45,4 @@ btnSignUp.addEventListener('click', e => {
   // Sign up
   const promise = auth.createUserWithEmailAndPassword(email, pass);
   promise.catch(e => console.log(e.message));
-});
+}
