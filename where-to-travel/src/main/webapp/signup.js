158d741a6f0e91efbac7e981ce11618d7f26f2b6
emmapaczkowski <emmapaczkowski@gmail.com>
2020-07-14 15:54:18 +0000

    Trying to merge my and andrews work on signup.js.  Currently not logging users in.

--- where-to-travel/src/main/webapp/signup.js
+++ where-to-travel/src/main/webapp/signup.js
@@ -2,7 +2,7 @@
 const emailSignUp = document.getElementById('emailSignUp');
 const passwordSignUp = document.getElementById('passwordSignUp');
 const passwordConfirmation = document.getElementById('passwordConfirmation');
-const btnSignUp = document.getElementById('signUp');
+const btnSignUp = document.getElementById('submit');
 
 // Preferred method of material form behavior interaction is jQuery
 // When the document is loaded, add validity check to form on submit.
@@ -32,14 +32,11 @@ function validatePassword(){
 passwordSignUp.onchange = validatePassword;
 passwordConfirmation.onkeyup = validatePassword;
 
-
-
 // Add signup event
-btnSignUp.addEventListener('click', e => {
+signUp = () => {
   // The user will be notified if it is invalid in the validate() function
-  if (validate() == false) {
-    return;
-  } else if(passwordSignUp.value.length < 6) {
+  validate();
+  if(passwordSignUp.value.length < 6) {
     alert("Your password must be at least 6 characters long. Please try again");
   } else {
     // Sign up
@@ -57,7 +54,7 @@ btnSignUp.addEventListener('click', e => {
     });
     promise.catch(e => console.log(e.message), alert(e.message));
   }
-});
+}
 
 // Verify that the email address is properly formatted using a regular expression.
 // A valid email is a string (a subset of ASCII characters) separated into two parts by an @ symbol. 
@@ -69,9 +66,9 @@ function validateEmail(email) {
 
 function validate() {
   if (validateEmail(emailSignUp.value)) {
-    return true;
+    return;
   } else {
     alert("is NOT a valid email address");
-    return false;
+    return;
   }
 }
