ac655315af89c0f36ea2e60018faf3400b73fe39
Priya Varra <priyavarra@google.com>
2020-07-14 17:03:30 +0000

    Change email and password length validation to form api

--- where-to-travel/src/main/webapp/signup.js
+++ where-to-travel/src/main/webapp/signup.js
@@ -2,7 +2,6 @@
 const emailSignUp = document.getElementById('emailSignUp');
 const passwordSignUp = document.getElementById('passwordSignUp');
 const passwordConfirmation = document.getElementById('passwordConfirmation');
-const btnSignUp = document.getElementById('submit');
 
 // Preferred method of material form behavior interaction is jQuery
 // When the document is loaded, add validity check to form on submit.
@@ -34,13 +33,7 @@ passwordConfirmation.onkeyup = validatePassword;
 
 // Add signup event
 signUp = () => {
-  // The user will be notified if it is invalid in the validate() function
-  validate();
-  if(passwordSignUp.value.length < 6) {
-    alert("Your password must be at least 6 characters long. Please try again");
-  } else {
-    // Sign up
-    const promise = firebase.auth().createUserWithEmailAndPassword(emailSignUp.value, passwordSignUp.value);
+  const promise = firebase.auth().createUserWithEmailAndPassword(emailSignUp.value, passwordSignUp.value);
     promise.then(e => {
       alert("You have sucessfully signed up!");
       
@@ -52,23 +45,7 @@ signUp = () => {
       }
       ref.push(data);
     });
-    promise.catch(e => console.log(e.message), alert(e.message));
+    promise.catch(e => console.log(e.message), alert(e.message)); 
   }
-}
 
-// Verify that the email address is properly formatted using a regular expression.
-// A valid email is a string (a subset of ASCII characters) separated into two parts by an @ symbol. 
-// The two parts being personal_info and a domain, that is personal_info@domain. 
-function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
 
-function validate() {
-  if (validateEmail(emailSignUp.value)) {
-    return;
-  } else {
-    alert("is NOT a valid email address");
-    return;
-  }
-}
